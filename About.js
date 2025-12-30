d7c28483217e62f54fc12f2f6812c0bffc4fa989
vrushanka1399 <vrushanka.1si17cs132@gmail.com>
2025-12-30 10:41:51 +0530

    Add files via upload

---
+++ About.js
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <div className="container mt-4">
+      <h1>About Us</h1>
+      <p>This is a demo ecommerce website.</p>
+    </div>
+  );
+};
+
+export default About;
--- App.js
+++ App.js
@@ -1,17 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
-import Products from './Products';
 import React from "react";
+import Products from './Products';
 import Cart from './Cart';
+
+import { Routes, Route, NavLink } from "react-router-dom";
+import About from "./About";
 
 function App() {
   return (
     <div>
-      {/* Cart button at the top */}
+
+      {/* Simple Navbar */}
+      <nav style={{ padding: '10px', background: '#f2f2f2' }}>
+        <NavLink to="/" style={{ marginRight: '20px' }}>
+          Home
+        </NavLink>
+
+        <NavLink to="/about">
+          About
+        </NavLink>
+      </nav>
+
+      {/* Cart button */}
       <Cart />
 
-      <h2 className="text-center mt-3">Products</h2>
-      <Products />
+      {/* Routes decide which page to show */}
+      <Routes>
+        <Route path="/" element={
+          <>
+            <h2 className="text-center mt-3">Products</h2>
+            <Products />
+          </>
+        } />
+
+        <Route path="/about" element={<About />} />
+      </Routes>
+
     </div>
   );
 }
